4fbad990db0c2f8ca450cd1cbc0996c5c44a40cd
vladimircalic <noliferop@gmail.com>
2021-04-22 21:08:54 +0200

    Added configuration to header

--- dashboard-task/src/components/Header.js
+++ dashboard-task/src/components/Header.js
@@ -1,9 +1,12 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 function Header() {
+  const configuration = useSelector((store) => store.configuration.configurationData);
+
   return (
     <div className="header">
-      <img src="https://img.innoloft.de/logo.svg" alt="logo" />
+      <img src={configuration.logo} alt="logo" style={{ color: configuration.mainColor }} />
     </div>
   );
 }
